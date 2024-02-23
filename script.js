9bbfa097adac96537539496e65e9b07047c9c7db
yunji Kim <y00ndlek@gmail.com>
2024-02-23 23:35:18 +0900

    test

--- script.js
+++ script.js
@@ -47,9 +47,7 @@ $(window).on("touchmove", function (e) {
   const deltaY = e.originalEvent.touches[0].clientY - startY;
   const deltaX = e.originalEvent.touches[0].clientX - startX;
   const scrollAmount = 100; // 스크롤 감도 조절
-  console.log(startX);
-  console.log(deltaX);
-  if (startX - deltaX > 100 || deltaX - startX > 100) {
+  if (startX - deltaX > 30 || deltaX - startX > 30) {
     isScrolling = false;
   }
 
