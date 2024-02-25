161e4f563e41f5fbf37f4b4d316e24adc5d83b93
yunji Kim <y00ndlek@gmail.com>
2024-02-25 12:26:56 +0900

    수정

--- script.js
+++ script.js
@@ -52,27 +52,40 @@ $(window).on("touchmove", function (e) {
   const remInnerHeight = inner[idx].clientHeight / htmlFontSize;
   if (remScrollDistance > remInnerHeight / 60) {
     idx = Math.max(idx - 1, 0);
-  } else if (-remScrollDistance > remInnerHeight / 60) {
+  } else if (-remScrollDistance > remInnerHeight / 0) {
     idx = Math.min(idx + 1, inner.length - 1);
   } else {
     idx = idx;
   }
 
-  $("html,body")
-    .stop()
-    .animate(
-      {
-        scrollTop: $(inner[idx]).offset().top, // 다음 페이지의 시작 위치로 스크롤
-      },
-      600,
-      function () {
-        isScrolling = false;
-      }
-    );
-  // }
+  debounce(() => {
+    $("html,body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(inner[idx]).offset().top, // 다음 페이지의 시작 위치로 스크롤
+        },
+        600,
+        function () {
+          isScrolling = false;
+        }
+      );
+  }, 600);
 
   // 스크롤 감쇠
   setTimeout(function () {
     isScrolling = false;
   }, 100);
 });
+
+function debounce(func, delay) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+}
