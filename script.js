abd0ec927a43220949364212e2bca652db79428d
yunji Kim <y00ndlek@gmail.com>
2024-03-15 00:41:00 +0900

    js 수정

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const swiper = new Swiper("#page2_swiper", {
 
 const swiper_background = new Swiper("#background", {
   direction: "vertical",
-  height: window.innerHeight,
+  slidesOffsetAfter: 0,
   // centerSlide
   slidesPerView: 1,
   centeredSlides: false,
