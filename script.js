3d526f6b5214239807690ad2a74cba4e94914cde
yunji Kim <y00ndlek@gmail.com>
2024-03-14 23:40:54 +0900

    전체 수정

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const swiper = new Swiper(".swiper", {
+const swiper_background = new Swiper("#background", {
+  direction: "vertical",
+
+  // centerSlide
+  slidesPerView: 1,
+  centeredSlides: false,
+  spaceBetween: 0,
+
+  //loop
+  loop: false,
+
+  //nextSlide
+  mousewheel: true,
+
+  //slideSpeed
+  speed: 1500,
+});
+
+const swiper = new Swiper("#page2_swiper", {
   // centerSlide
   slidesPerView: "1.1",
   centeredSlides: false,
@@ -14,7 +32,6 @@ const swiper = new Swiper(".swiper", {
   speed: 1500,
 });
 
-//배경 스크롤
 window.addEventListener("scroll", function () {
   const image = document.getElementById("arrow");
   const scrollPosition = window.scrollY;
@@ -27,52 +44,4 @@ window.addEventListener("scroll", function () {
     image.style.transition = "visibility 0.1s";
     image.style.visibility = "visible";
   }
-});
-
-let idx = 0;
-let startY;
-let isScrolling = false;
-
-const select = document.querySelector(".background");
-const inner = select.querySelectorAll(".page1, .page2, .page3, .page4");
-
-$(window).on("touchstart", function (e) {
-  startX = e.originalEvent.touches[0].clientX;
-  startY = e.originalEvent.touches[0].clientY;
-});
-
-$(window).on("touchmove", function (e) {
-  if (isScrolling) return; // 이미 스크롤 중이면 무시
-  isScrolling = true;
-
-  const scrollDistance = e.originalEvent.changedTouches[0].clientY - startY;
-
-  const htmlFontSize = parseFloat($("html").css("font-size"));
-  const remScrollDistance = scrollDistance / htmlFontSize;
-  const remInnerHeight = inner[idx].clientHeight / htmlFontSize;
-
-  if (remScrollDistance > remInnerHeight / 60) {
-    idx = Math.max(idx - 1, 0);
-  } else if (-remScrollDistance > remInnerHeight / 60) {
-    idx = Math.min(idx + 1, inner.length - 1);
-  } else {
-    idx = idx;
-  }
-
-  $("html,body")
-    .stop()
-    .animate(
-      {
-        scrollTop: $(inner[idx]).offset().top, // 다음 페이지의 시작 위치로 스크롤
-      },
-      600,
-      function () {
-        isScrolling = false;
-      }
-    );
-
-  // 스크롤 감쇠
-  setTimeout(function () {
-    isScrolling = false;
-  }, 100);
 });
