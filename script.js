cf3ab0edcbc1b69540143612a6cc725a46f67b04
yunji Kim <y00ndlek@gmail.com>
2024-03-20 12:06:31 +0900

    수정

--- script.js
+++ script.js
@@ -51,17 +51,3 @@ const swiper = new Swiper("#page2_swiper", {
   //slideSpeed
   speed: 1500,
 });
-
-// window.addEventListener("scroll", function () {
-//   const image = document.getElementById("arrow");
-//   const scrollPosition = window.scrollY;
-//   const bottomOfPage = document.body.scrollHeight - window.innerHeight;
-
-//   if (scrollPosition >= bottomOfPage - 5) {
-//     image.style.transition = "visibility 0.1s";
-//     image.style.visibility = "hidden";
-//   } else {
-//     image.style.transition = "visibility 0.1s";
-//     image.style.visibility = "visible";
-//   }
-// });
